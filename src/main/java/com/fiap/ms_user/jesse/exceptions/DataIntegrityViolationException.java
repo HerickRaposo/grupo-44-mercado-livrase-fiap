47ab8b8217587627203893570e5c166487031fde
jessemusic <jmc_cursos@hotmail.com>
2024-03-07 14:34:29 -0300

    Lógica de usuário já cadastrado na base e tratamento de exception

---
+++ src/main/java/com/fiap/ms_user/jesse/exceptions/DataIntegrityViolationException.java
@@ -0,0 +1,14 @@
+package com.fiap.ms_user.jesse.exceptions;
+
+public class DataIntegrityViolationException extends RuntimeException{
+    public DataIntegrityViolationException() {
+    }
+
+    public DataIntegrityViolationException(String message) {
+        super(message);
+    }
+
+    public DataIntegrityViolationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
